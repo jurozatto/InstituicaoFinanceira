141981d54fc267510afe1c920e5f43eb7495f0e4
jurozatto <juliarozatto6@gmail.com>
2025-09-30 20:26:59 -0300

    conta com numero e saldo

---
+++ ControleContas/Conta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ControleContas
+{
+    public class Conta
+    {
+        public Conta(long numero) //construtor default (mesmo nome da classe), a assinatura deste e do segundo é diferente, esse recebe long numero e o outro nada | não tem retorno pois constroi a classe
+        {
+            this.Numero = numero;
+        }
+
+        public Conta()
+        {
+            _numero = new Random().Next(100000000, 999999999);
+        }
+
+        
+        private long _numero; //long é um tipo inteiro maior | com private significa que o número da conta pode ser acessado somente dentro da classe (dá erro na classe)
+        public long Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            private set
+            {
+                _numero = value;
+            }
+        }
+
+
+            public decimal _saldo;
+            public decimal Saldo
+            {
+                get
+                {
+                    return _saldo;
+                }
+                private set
+                {
+                    _saldo = value;
+                }
+
+            }
+      
+        
+    }
+}
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ControleContas;
+using System;
+
+Conta conta1 = new Conta(123456789);
+Conta conta2 = new Conta();
+Console.WriteLine($"O número da conta 1 é {conta1.Numero}");
+Console.WriteLine($"O número da conta 2 é {conta2.Numero}");
+
+
+//crie um atributo saldo na classe Conta cuja propriedade seja somente leitura
