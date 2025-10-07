c5337aeaacabb75d1b58c44d3acaf69bfbe5ea18
jurozatto <juliarozatto6@gmail.com>
2025-10-06 22:37:01 -0300

    slide 26

--- ControleContas/Conta.cs
+++ ControleContas/Conta.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,12 @@ namespace ControleContas
 {
     public class Conta
     {
+        private static decimal _saldoTotalGeral = 0;
+        private static Conta _contaMaiorSaldo = null;
+
+
+
+
         public Conta(long numero) //construtor default (mesmo nome da classe), a assinatura deste e do segundo é diferente, esse recebe long numero e o outro nada | não tem retorno pois constroi a classe
         {
             this.Numero = numero;
@@ -57,10 +64,49 @@ namespace ControleContas
                 return;
             }
             _saldo += value;
+            _saldoTotalGeral += value;
+
+            AtualizarContaMaiorSaldo();
         }
-      
-        
+
+        public static decimal SaldoTotalGeral
+        {
+            get { return _saldoTotalGeral; }
+        }
+
+        public static Conta ContaMaiorSaldo
+        {
+            get { return _contaMaiorSaldo; }
+        }
+
+        private void AtualizarContaMaiorSaldo()
+        {
+            if (_contaMaiorSaldo == null || this._saldo > _contaMaiorSaldo._saldo)
+            {
+                _contaMaiorSaldo = this;
+            }
+        }
+
+        public Cliente Titular { get; private set; }
+
+
+        // Construtor com cliente
+        public Conta(long numero, Cliente titular)
+        {
+            this.Numero = numero;
+            this.Titular = titular ?? throw new ArgumentNullException(nameof(titular), "A conta precisa de um titular.");
+        }
+
+        public Conta(Cliente titular)
+        {
+            _numero = new Random().Next(100000000, 999999999);
+            this.Titular = titular ?? throw new ArgumentNullException(nameof(titular), "A conta precisa de um titular.");
+        }
+
     }
+
+
 }
 
-//oii
+
+
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -1,10 +1,32 @@
 ﻿using ControleContas;
 using System;
 
-Conta conta1 = new Conta(123456789);
-Conta conta2 = new Conta();
-Console.WriteLine($"O número da conta 1 é {conta1.Numero}");
-Console.WriteLine($"O número da conta 2 é {conta2.Numero}");
+Cliente cliente1 = new Cliente("Júlia", "12345678901", 2005);
+cliente1.MostrarDados();
+
+Conta conta1 = new Conta(123456789, cliente1);
+Conta conta2 = new Conta(654321, cliente1);
+
+conta1.Depositar(1000m);
+conta2.Depositar(2341.42m);
+
+Console.WriteLine($"\nTitular: {cliente1.Nome} (CPF: {cliente1.CPF})");
+Console.WriteLine($"Conta 1: número = {conta1.Numero}, saldo = {conta1.Saldo:C}");
+Console.WriteLine($"Conta 2: número = {conta2.Numero}, saldo = {conta2.Saldo:C}");
+
+
+
+decimal totalContas = conta1.Saldo + conta2.Saldo;
+Console.WriteLine($"\nSaldo total das duas contas: {totalContas:C}");
+
+
+//Conta com maior saldo
+Console.WriteLine($"\nConta com maior saldo: {Conta.ContaMaiorSaldo.Numero} (Saldo: {Conta.ContaMaiorSaldo.Saldo:C})");
+
+
+// Saldo total geral
+Console.WriteLine($"\nSaldo total geral atual: {Conta.SaldoTotalGeral:C}");
+
 
 
 //crie um atributo saldo na classe Conta cuja propriedade seja somente leitura
